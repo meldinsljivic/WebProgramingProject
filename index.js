c4a4ace067700e616d196e4510770e358542a1d5
meldinsljivic <meldin.sljivic@gmail.com>
2018-05-19 13:49:23 +0200

    mongo

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const bodyparser = require("body-parser");
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const jwt_secret = 'WU5CjF8fHxG40S2t7oyk';
+
+var jwt    = require('jsonwebtoken');
+var MongoId = require('mongodb').ObjectID;
+var db;
+
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
 
 app.use('/', express.static('static'));
 
-app.listen(3000, () => console.log('slusa na 3000'));
+
+
+
+MongoClient.connect('mongodb://localhost:27017/webprograming', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => console.log('Example app listening on port 3000!'))
+})
+

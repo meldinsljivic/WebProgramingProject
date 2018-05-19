8fe39ebb05f6a49c48f49e9faaa51dfa8d6ae512
meldinsljivic <meldin.sljivic@gmail.com>
2018-05-19 14:10:24 +0200

    register

--- index.js
+++ index.js
@@ -13,7 +13,40 @@ app.use(express.urlencoded()); // to support URL-encoded bodies
 
 app.use('/', express.static('static'));
 
+app.post('/login', function(request, response){
+    var user = request.body;
+  
+    db.collection("users").findOne({'username': user.username, 'password': user.password}, function(error, user) {
+      if (error){
+        throw error;
+      }else{
+        if(user){
+          var token = jwt.sign(user, jwt_secret, {
+            expiresIn: 20000 
+          });
+      
+          response.send({
+            success: true,
+            message: 'Authenticated',
+            token: token
+          })
+        }else{
+          response.status(401).send('Credentials are wrong.');
+        }
+      }
+    });
+  });
 
+
+  app.post('/register', function(req, res) {
+    req.body._id = null;
+    var user = req.body;
+    database.collection('users').insert(user, function(err, data) {
+        if (err) return console.log(err);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(user);
+    })
+});
 
 
 MongoClient.connect('mongodb://localhost:27017/webprograming', (err, database) => {
